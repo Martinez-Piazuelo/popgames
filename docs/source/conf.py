70da69b21b8472f9c09b21027d6831de995a1faf
juan <jp.martinez10@uniandes.edu.co>
2025-06-21 18:13:47 +0200

    Print path in deployment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
+print("PYTHONPATH during Sphinx build:", sys.path)
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
