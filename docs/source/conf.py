7e8fa9c2bc6dcbe00303e7d9a91f3f204baab69d
juan <jp.martinez10@uniandes.edu.co>
2025-06-21 17:45:27 +0200

    Add automated docs config

---
+++ docs/source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'popgames'
+copyright = '2025, Juan Martinez-Piazuelo'
+author = 'Juan Martinez-Piazuelo'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+autodoc_typehints = 'description'
+autoclass_content = 'both'  # shows class docstring AND __init__ docstring
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+    'inherited-members': True,
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/source/revision_protocol.rst
@@ -0,0 +1,8 @@
+Revision Protocol Module
+========================
+
+.. automodule:: popgames.revision_protocol
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :special-members: __init__, __call__
